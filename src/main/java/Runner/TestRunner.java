3550566ec9903902e8b622495a196c1d347d7d2e
raghu <raghu@raghu-HP>
2018-05-25 20:18:11 +0530

    cmt 3

--- src/main/java/Runner/TestRunner.java
+++ src/main/java/Runner/TestRunner.java
@@ -20,7 +20,7 @@ import cucumber.api.junit.Cucumber;
 		plugin = { "com.cucumber.listener.ExtentCucumberFormatter:src\\main\\java\\com\\test\\automation\\reports\\report.html"},
 		tags = { "@SmokeTests,@UATTests" }
 		
-		//monochrome = true 
+		//monochrome = true
 		)
 
 public class TestRunner {
